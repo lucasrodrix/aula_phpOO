3c0389578f00f40ec8e777545830104608e82874
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-05-30 16:32:10 -0300

    Trbalhando com Banco de Dados e PDO

---
+++ Store/Produtos.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+class Produto{
+    private $conn;
+
+    public function __construct(){
+        try {
+            $this->conn = new PDO("pgsql:host=localhost;port=5432;dbname=store;user=postgres;password=rodrix3301");
+        } catch (Exception $e){
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    public function listar():array{
+        $sql = 'SELECT id, descricao from produtos ORDER BY id ASC';
+        $produtos = [];
+
+        foreach ($this->conn->query($sql) as $key => $value) {
+            array_push($produtos, $value);
+        }
+        return $produtos;
+    }
+
+    public function inserir(int $id, string $descricao){
+        $sql = 'INSERT INTO produtos (id, descricao) VALUES (?,?)';
+
+        $prepare = $this->conn->prepare($sql);
+        
+        $prepare->bindParam(1, $id);
+        $prepare->bindParam(2, $descricao);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+    
+    public function atualizar(string $descricao, int $id){
+        $sql = 'UPDATE produtos SET descricao =? WHERE id = ?';
+
+        $prepare = $this->conn->prepare($sql);
+        
+        $prepare->bindParam(1, $descricao);
+        $prepare->bindParam(2, $id);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+
+    public function deletar(int $id){
+        $sql = 'DELETE FROM produtos WHERE id = ?';
+
+        $prepare = $this->conn->prepare($sql);
+        
+        $prepare->bindParam(1, $id);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+
+}
---
+++ store.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+require ".\Store\Produtos.php";
+$produto = new Produto();
+
+switch ($$_GET['operacao']) {
+    case 'listar':
+        foreach ($$produto->listar() as $key => $value) {
+            echo 'ID: '.$value['id'].'</br> Descrição: '.$value['descricao'].'<hr>';
+        }
+    break;
+
+    case 'inserir':
+        $status = $produto->inserir(1, 'PC POSITIVO');
+
+        if(!$status){
+            echo "Não foi possível realizar a operação!";
+            return false;
+        }
+        echo "Registro Inserido com Sucesso!";
+    break;
+
+    case 'atualizar':
+        $status = $produto->atualizar('PC LENOVO',1);
+
+        if(!$status){
+            echo "Não foi possível realizar a operação!";
+            return false;
+        }
+        echo "Registro Atualizado com Sucesso!";
+    break;
+
+    case 'deletar':
+        $status = $produto->deletar(3);
+
+        if(!$status){
+            echo "Não foi possível realizar a operação!";
+            return false;
+        }
+        echo "Registro Removido com Sucesso!";
+    break;
+}
